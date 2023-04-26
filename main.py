599a5801d047c04ec044a0c78f393ba7f1d6c5d2
saurivane <saurivane@gmail.com>
2023-04-26 09:51:51 +0200

    pequeña corrección en main para la salida del print

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ numero = int(input('Introduce el número a convertir entre 0 y 1999: '))
 
 x = romanos.romanos(numero)
 
-print(f"El numero " + str(numero) + ", en números romanos es: " + str(x))
+print(f"El numero " + str(numero) + " en números romanos es: " + str(x))
